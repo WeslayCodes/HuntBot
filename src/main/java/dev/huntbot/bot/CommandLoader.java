7bff5df01d7298033c89e11f97695e8627b30463
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2025-04-27 14:20:37 -0500

    debugging

--- src/main/java/dev/huntbot/bot/CommandLoader.java
+++ src/main/java/dev/huntbot/bot/CommandLoader.java
@@ -28,16 +28,19 @@ class CommandLoader implements Configured {
             guildCommands.add(SlashCommandData.fromData(DataObject.fromJson(command.toString())));
         }
 
+        Log.debug("1");
+
         Guild guild = HuntBotApp.getBot().getJDA().getGuildById(CONFIG.getMainConfig().getGuild());
 
-        HuntBotApp.getBot().getJDA().updateCommands().addCommands().queue();
+        Log.debug("2");
 
         if (guild != null) {
-            guild.updateCommands().addCommands().queue();
+            Log.debug("3");
             guild.updateCommands().addCommands(guildCommands).queue(null, e -> {
                 Log.error(CommandLoader.class, "Failed to deploy to development guild", e);
                 System.exit(-1);
             });
+            Log.debug("4");
         } else {
             Log.error(
                 CommandLoader.class, "Unable to find guild. Could not deploy commands!", new IllegalArgumentException()
