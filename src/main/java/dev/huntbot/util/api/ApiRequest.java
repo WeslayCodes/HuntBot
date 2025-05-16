e3052cf6db92e76172470cb05de80789600a3854
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2025-05-15 21:27:35 -0500

    Better logs

--- src/main/java/dev/huntbot/util/api/ApiRequest.java
+++ src/main/java/dev/huntbot/util/api/ApiRequest.java
@@ -66,6 +66,10 @@ public class ApiRequest implements Configured {
             .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+        if (response.statusCode() != 200) {
+            Log.warn(ApiRequest.class, "Non 200 status code returned: " + response);
+        }
+
         Log.info(ApiRequest.class, "Hit Gemini API with status code " + response.statusCode());
 
         return JsonParser.parseString(response.body()).getAsJsonObject();
